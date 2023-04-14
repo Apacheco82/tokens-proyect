e6df8e47c9094723d6ded4ec39cf38bf42355b65
Ana Pacheco <kornetera@gmail.com>
2023-04-14 07:47:46 +0000

    react

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,19 +8,23 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+			<form>
+				<div class="form-group">
+					<label for="exampleInputEmail1">Email address</label>
+					<input type="email" name="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email"></input>
+						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+				</div>
+				<div class="form-group">
+					<label for="exampleInputEmail1">User name</label>
+					<input type="text" name= "user_name"class="form-control"  aria-describedby="emailHelp" placeholder="Enter user name"></input>
+				</div>
+				<div class="form-group">
+					<label for="exampleInputPassword1">Password</label>
+					<input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+				</div>
+			
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
 		</div>
 	);
 };
