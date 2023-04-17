461a72dddfa00ef5e7075a5119c234070adcf338
Ana Pacheco <kornetera@gmail.com>
2023-04-17 11:18:11 +0000

    listo para entregar sin front

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,20 +3,17 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-<nav className="navbar navbar-expand-xl navbar-dark bg-dark">
-  <div className="container">
-    <Link to="/" className="link-styling">
-      <h1 className="navbar-brand mb-0 h1 text-warning">Únete a nuestro club</h1>
-    </Link>
-    <div className="ml-auto">
-      <Link to="/Formulario">
-        <button className="btn btn-primary">Check the Context in action</button>
-      </Link>
-    </div>
-  </div>
-</nav>
-
-
-
+		<nav className="navbar navbar-light bg-light">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				</Link>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
+				</div>
+			</div>
+		</nav>
 	);
 };
